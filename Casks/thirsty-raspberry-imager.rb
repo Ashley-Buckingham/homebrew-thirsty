3e1402deddf1459d78a19118498774f3d6b79b35
Cur50r <ashleyjgbuckingham@googlemail.com>
2022-12-08 02:24:06 +0000

    added raspberry imager

--- Casks/thirsty-raspberry-imager.rb
+++ Casks/thirsty-raspberry-imager.rb
@@ -1,5 +1,5 @@
   cask "thirsty-raspberry-imager" do
-    version "1.1.1"
+    version "1.7.3"
     sha256 "f4f6d81c5103b065bffd53e316f868797369ecedd47257c1620d6efca5d3d593"
 
     url "https://downloads.raspberrypi.org/imager/imager_latest.dmg"
@@ -7,7 +7,7 @@
     desc "Raspberry Pi Imager is an application to write Raspberry Pi OS images to an SD card"
     homepage "https://www.raspberrypi.com"
 
-    app "imager_1.7.3.dmg"
+    app "imager_1.7.3.pkg"
 
     zap trash: [
     #   "~/Library/Application Support/com.postmanlabs.mac.ShipIt",
