9733c4172a8af16b6a6ea18ec037c2e8b6991bf2
Cur50r <ashleyjgbuckingham@googlemail.com>
2022-12-08 19:14:47 +0000

    adding script

--- Formula/thirsty-brew-finder.rb
+++ Formula/thirsty-brew-finder.rb
@@ -9,7 +9,7 @@ class ThirstyBrewFinder < Formula
 
     def install
     #   system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-      bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install "thirsty-brew-finder"
     end
 
     test do
