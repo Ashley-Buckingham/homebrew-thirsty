c37948b74f28d92e4ac072388126d1a5674955df
Cur50r <ashleyjgbuckingham@googlemail.com>
2022-12-08 02:06:41 +0000

    added raspberry imager

---
+++ Casks/raspberry-imager.rb
@@ -0,0 +1,22 @@
+  cask "raspberry-imager" do
+    version "1.1.1"
+    # sha256 "89be94a18c507f2cf7f8e6e627d8f2ed16dff6b236437585aa2af04ef4cc2698"
+
+    url "https://downloads.raspberrypi.org/imager/imager_latest.dmg"
+    name "Raspberry Imager"
+    desc "Raspberry Pi Imager is an application to write Raspberry Pi OS images to an SD card"
+    homepage "https://www.raspberrypi.com"
+
+    app "imager.pkg"
+
+    zap trash: [
+    #   "~/Library/Application Support/com.postmanlabs.mac.ShipIt",
+    #   "~/Library/Application Support/Postman",
+    #   "~/Library/Caches/com.postmanlabs.mac.ShipIt",
+    #   "~/Library/Caches/com.postmanlabs.mac",
+    #   "~/Library/Caches/Postman",
+    #   "~/Library/Preferences/ByHost/com.postmanlabs.mac.ShipIt.*.plist",
+    #   "~/Library/Preferences/com.postmanlabs.mac.plist",
+    #   "~/Library/Saved Application State/com.postmanlabs.mac.savedState",
+    ]
+  end
