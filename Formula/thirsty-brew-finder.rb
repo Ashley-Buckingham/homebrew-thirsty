c92b8fac51b617e93932fb5d47599231c0538d62
Cur50r <ashleyjgbuckingham@googlemail.com>
2022-12-08 19:12:03 +0000

    adding script

--- Formula/thirsty-brew-finder.rb
+++ Formula/thirsty-brew-finder.rb
@@ -8,7 +8,7 @@ class ThirstyBrewFinder < Formula
     depends_on "node"
 
     def install
-      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    #   system "npm", "install", *Language::Node.std_npm_install_args(libexec)
       bin.install_symlink Dir["#{libexec}/bin/*"]
     end
 
