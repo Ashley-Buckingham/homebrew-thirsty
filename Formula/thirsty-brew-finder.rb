1897370b2a4546688b7b61dfc961c5e303aab9ce
Cur50r <ashleyjgbuckingham@googlemail.com>
2022-12-09 18:58:19 +0000

    adding sha

--- Formula/thirsty-brew-finder.rb
+++ Formula/thirsty-brew-finder.rb
@@ -3,7 +3,7 @@ class ThirstyBrewFinder < Formula
   desc "This script finds the cask for all apps that are not installed by brew on the machine"
   homepage "https://github.com/Cur50r/homebrew-thirsty"
   url "https://github.com/Cur50r/homebrew-thirsty/archive/refs/tags/0.0.1-ThirstyBrewFinder.tar.gz"
-  sha256 "b06bed6de107f29c71533731550ca979dafa87d96b12e22a65bd1a6c3ef40d98"
+  sha256 "4383423e4e99724633398b55c66f983698b485d08873bfd1c3248aa2b19d78ee"
 
   def install
     bin.install "thirstybrewfinder"
